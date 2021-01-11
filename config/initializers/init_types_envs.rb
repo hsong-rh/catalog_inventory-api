bc23493c436e977343968393f39aec2e154b4993
hsong-rh <hsong@redhat.com>
2021-01-11 14:19:31 -0500

    Fix travis build failure

--- config/initializers/init_types_envs.rb
+++ config/initializers/init_types_envs.rb
@@ -1,18 +1,21 @@
 module InitTypesEnvs
-  SOURCE_TYPE_NAME = "ansible-tower".freeze
-  APPLICATION_TYPE_NAME = "/insights/platform/catalog".freeze
+  # skip the initializer code if in travis build
+  unless ENV["TRAVIS"]
+    SOURCE_TYPE_NAME = "ansible-tower".freeze
+    APPLICATION_TYPE_NAME = "/insights/platform/catalog".freeze
 
-  source_types = Sources::Service.call do |api_instance|
-    api_instance.list_source_types(:filter => {:name => SOURCE_TYPE_NAME})
+    source_types = Sources::Service.call do |api_instance|
+      api_instance.list_source_types(:filter => {:name => SOURCE_TYPE_NAME})
+    end
+
+    application_types = Sources::Service.call do |api_instance|
+      api_instance.list_application_types(:filter => {:name => APPLICATION_TYPE_NAME})
+    end
+
+    source_type_id = source_types.try(:data).try(:first).try(:id)
+    application_type_id = application_types.try(:data).try(:first).try(:id)
+
+    ENV["SOURCE_TYPE_ID"]      = source_type_id
+    ENV["APPLICATION_TYPE_ID"] = application_type_id
   end
-
-  application_types = Sources::Service.call do |api_instance|
-    api_instance.list_application_types(:filter => {:name => APPLICATION_TYPE_NAME})
-  end
-
-  source_type_id = source_types.try(:data).try(:first).try(:id)
-  application_type_id = application_types.try(:data).try(:first).try(:id)
-
-  ENV["SOURCE_TYPE_ID"]      = source_type_id
-  ENV["APPLICATION_TYPE_ID"] = application_type_id
 end
