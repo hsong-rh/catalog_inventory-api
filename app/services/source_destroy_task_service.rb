ad01bab73df639552808f581a4ad650580ba824c
Madhu Kanoor <mkanoor@redhat.com>
2021-01-22 21:08:36 -0500

    Same thing when a source needs to be deleted

--- app/services/source_destroy_task_service.rb
+++ app/services/source_destroy_task_service.rb
@@ -7,12 +7,12 @@ class SourceDestroyTaskService
     return if ClowderConfig.instance["SOURCE_TYPE_ID"].blank? || ClowderConfig.instance["SOURCE_TYPE_ID"] != @options[:source_type_id]
 
     validate_options
-    Source.destroy(@options[:source_id].to_i)
+    Source.destroy(@options[:id].to_i)
   end
 
   private
 
   def validate_options
-    raise("Options must have source_id key") if @options[:source_id].blank?
+    raise("Options must have id key") if @options[:id].blank?
   end
 end
