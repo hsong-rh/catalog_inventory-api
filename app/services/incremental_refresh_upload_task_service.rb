38388c295f573e54a7f123b175061da647e81bad
Madhu Kanoor <mkanoor@redhat.com>
2021-01-26 17:56:58 -0500

    Add order=id in Ansible Tower Requests
    This allows us to expect the data in the same order everytime from
    the tower

--- app/services/incremental_refresh_upload_task_service.rb
+++ app/services/incremental_refresh_upload_task_service.rb
@@ -53,7 +53,7 @@ class IncrementalRefreshUploadTaskService < TaskService
   def templates_jobs
     [].tap do |templates_jobs|
       templates_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/job_templates?modified__gt=#{@last_successful_refresh_at}"
+        job.href_slug = "#{TOWER_API_VERSION}/job_templates?order=id&modified__gt=#{@last_successful_refresh_at}"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id, type:type, url:url,created:created,name:name, modified:modified, description:description,survey_spec:related.survey_spec,inventory:related.inventory,survey_enabled:survey_enabled,ask_tags_on_launch:ask_tags_on_launch,ask_limit_on_launch:ask_limit_on_launch,ask_job_type_on_launch:ask_job_type_on_launch,ask_diff_mode_on_launch:ask_diff_mode_on_launch,ask_inventory_on_launch:ask_inventory_on_launch,ask_skip_tags_on_launch:ask_skip_tags_on_launch,ask_variables_on_launch:ask_variables_on_launch,ask_verbosity_on_launch:ask_verbosity_on_launch,ask_credential_on_launch:ask_credential_on_launch}"
@@ -61,7 +61,7 @@ class IncrementalRefreshUploadTaskService < TaskService
       end
 
       templates_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/job_templates/"
+        job.href_slug = "#{TOWER_API_VERSION}/job_templates/?order=id"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id}"
@@ -74,14 +74,14 @@ class IncrementalRefreshUploadTaskService < TaskService
   def credentials_jobs
     [].tap do |credentials_jobs|
       credentials_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/credentials?modified__gt=#{@last_successful_refresh_at}"
+        job.href_slug = "#{TOWER_API_VERSION}/credentials?order=id&modified__gt=#{@last_successful_refresh_at}"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id, type:type, created:created, name:name, modified:modified, description:description, credential_type:credential_type}"
       end
 
       credentials_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/credentials/"
+        job.href_slug = "#{TOWER_API_VERSION}/credentials/?order=id"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id}"
@@ -93,14 +93,14 @@ class IncrementalRefreshUploadTaskService < TaskService
   def credential_types_jobs
     [].tap do |credential_types_jobs|
       credential_types_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/credential_types?modified__gt=#{@last_successful_refresh_at}"
+        job.href_slug = "#{TOWER_API_VERSION}/credential_types??order=id&modified__gt=#{@last_successful_refresh_at}"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id, type:type, created:created, name:name, modified:modified, description:description, kind:kind, namespace:namespace}"
       end
 
       credential_types_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/credential_types/"
+        job.href_slug = "#{TOWER_API_VERSION}/credential_types/?order=id"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id}"
@@ -112,14 +112,14 @@ class IncrementalRefreshUploadTaskService < TaskService
   def inventories_jobs
     [].tap do |inventories_jobs|
       inventories_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/inventories?modified__gt=#{@last_successful_refresh_at}"
+        job.href_slug = "#{TOWER_API_VERSION}/inventories?order=id&modified__gt=#{@last_successful_refresh_at}"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id, type:type, created:created, name:name, modified:modified, description:description, kind:kind, type:type, variables:variables, host_filter:host_filter, pending_deletion:pending_deletion, organization:organization, inventory_sources_with_failures:inventory_sources_with_failures}"
       end
 
       inventories_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/inventories/"
+        job.href_slug = "#{TOWER_API_VERSION}/inventories/?order=id"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id}"
@@ -131,7 +131,7 @@ class IncrementalRefreshUploadTaskService < TaskService
   def workflow_templates_jobs
     [].tap do |workflow_templates_jobs|
       workflow_templates_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/workflow_job_templates?modified__gt=#{@last_successful_refresh_at}"
+        job.href_slug = "#{TOWER_API_VERSION}/workflow_job_templates?order=id&modified__gt=#{@last_successful_refresh_at}"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id, type:type, url:url,created:created, name:name, modified:modified, description:description, survey_spec:related.survey_spec, inventory:related.inventory, survey_enabled:survey_enabled, ask_inventory_on_launch:ask_inventory_on_launch, ask_variables_on_launch:ask_variables_on_launch}"
@@ -139,7 +139,7 @@ class IncrementalRefreshUploadTaskService < TaskService
       end
 
       workflow_templates_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/workflow_job_templates/"
+        job.href_slug = "#{TOWER_API_VERSION}/workflow_job_templates/?order=id"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id}"
@@ -151,14 +151,14 @@ class IncrementalRefreshUploadTaskService < TaskService
   def workflow_template_nodes_jobs
     [].tap do |workflow_template_nodes_jobs|
       workflow_template_nodes_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/workflow_job_template_nodes?modified__gt=#{@last_successful_refresh_at}"
+        job.href_slug = "#{TOWER_API_VERSION}/workflow_job_template_nodes?order=id&modified__gt=#{@last_successful_refresh_at}"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id, unified_job_type:summary_fields.unified_job_template.unified_job_type, inventory:inventory, type:type, url:url, created:created, modified:modified, workflow_job_template:workflow_job_template, unified_job_template:unified_job_template}"
       end
 
       workflow_template_nodes_jobs << CatalogInventory::Job.new.tap do |job|
-        job.href_slug = "#{TOWER_API_VERSION}/workflow_job_template_nodes/"
+        job.href_slug = "#{TOWER_API_VERSION}/workflow_job_template_nodes/?order=id"
         job.method = "GET"
         job.fetch_all_pages = true
         job.apply_filter = "results[].{id:id}"
