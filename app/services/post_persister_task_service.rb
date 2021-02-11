c099a6ebd471e34cf9e23467236ffd97aa4c6e27
Madhu Kanoor <mkanoor@redhat.com>
2021-02-11 16:28:22 -0500

    Formatting of last refresh message
    Allow the UI to show the last refresh stats about how many objects
    were added, updated, deleted.

--- app/services/post_persister_task_service.rb
+++ app/services/post_persister_task_service.rb
@@ -21,13 +21,30 @@ class PostPersisterTaskService < TaskService
   def ok_options
     {:last_successful_refresh_at => @options[:task][:input]["refresh_request_at"],
      :refresh_finished_at        => Time.current,
-     :last_refresh_message       => @options[:task][:output],
+     :last_refresh_message       => refresh_stats_message,
      :refresh_state              => "Done"}
   end
 
   def error_options
     {:refresh_finished_at  => Time.current,
-     :last_refresh_message => @options[:task][:output][:errors].join("; "),
+     :last_refresh_message => @options[:task][:output]["errors"].join("\n"),
      :refresh_state        => "Error"}
+  end
+
+  def refresh_stats_message
+    message = ""
+    @options[:task][:output].fetch("stats", {}).each do |obj, counters|
+       hash = counters.select { |key, value| value > 0 }
+       next if hash.empty?
+       if message != ""
+          message += "\n"
+       end
+       message += "#{obj}: "
+       message += hash.map{|k,v| "#{k}=#{v}"}.join(',')
+    end
+    if message == ""
+       message = "No updates"
+    end
+    message
   end
 end
---
+++ spec/services/post_persister_task_service_spec.rb
@@ -0,0 +1,57 @@
+describe PostPersisterTaskService do
+  include ::Spec::Support::TenantIdentity
+
+  let(:source) { FactoryBot.create(:source, :tenant => tenant) }
+  let(:input) { { "refresh_request_at" => Time.current} }
+  let(:status) { 'ok' }
+  let(:task) { FactoryBot.create(:task, :source => source, :tenant => tenant, :input => input, :output => output, :status => status) }
+  let(:params) { {'source_id' => source.id, 'tenant_id' => tenant.id, 'task' => task} }
+  let(:subject) { described_class.new(params) }
+  let(:refresh_state) { "Done" }
+
+  around do |example|
+    Insights::API::Common::Request.with_request(default_request) { example.call }
+  end
+
+  shared_examples_for "#process status ok" do
+    it "should update the source object" do
+      subject.process
+
+      source.reload
+      expect(source.last_refresh_message).to eq(last_refresh_message)
+      expect(source.refresh_state).to eq(refresh_state)
+    end
+  end
+
+  context "No updates" do
+    let(:output) { {} }
+    let(:last_refresh_message) { "No updates" }
+
+    it_behaves_like "#process status ok"
+  end
+
+  context "Zeroed stats" do
+    let(:stats) { {'service_plans' => {'adds' => 0, 'deletes' => 0}, 'creds' => {'adds' => 0, 'updates' => 0, 'deletes' => 0}}}
+    let(:output) { {"stats" => stats} }
+    let(:last_refresh_message) { "No updates" }
+
+    it_behaves_like "#process status ok"
+  end
+
+  context "Show non zero values" do
+    let(:stats) { {'service_plans' => {'adds' => 0, 'deletes' => 2}, 'creds' => {'adds' => 0, 'updates' => 0, 'deletes' => 0}, 'inventories' => {'adds' => 9}}}
+    let(:output) { {"stats" => stats} }
+    let(:last_refresh_message) { "service_plans: deletes=2\ninventories: adds=9"}
+
+    it_behaves_like "#process status ok"
+  end
+
+  context "Show non zero values" do
+    let(:status) { 'error' }
+    let(:output) { {"errors" => ["kaboom","failed miserably"]} }
+    let(:last_refresh_message) { "kaboom\nfailed miserably"}
+    let(:refresh_state) { "Error" }
+
+    it_behaves_like "#process status ok"
+  end
+end
