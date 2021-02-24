6ea65e735e8b4ca0a11d6427374584072d0813d7
hsong-rh <hsong@redhat.com>
2021-02-24 14:51:18 -0500

    Added more destroy dependents

--- app/models/source.rb
+++ app/models/source.rb
@@ -7,22 +7,22 @@ class Source < ApplicationRecord
   acts_as_tenant(:tenant)
 
   # Service Catalog Inventory Objects
-  has_many :service_credential_types
-  has_many :service_credentials
+  has_many :service_credential_types, :dependent => :destroy
+  has_many :service_credentials, :dependent => :destroy
   has_many :service_offerings, :dependent => :destroy
-  has_many :service_offering_nodes
-  has_many :service_offering_icons
+  has_many :service_offering_nodes, :dependent => :destroy
+  has_many :service_offering_icons, :dependent => :destroy
   has_many :service_offering_service_credentials, :through => :service_offerings
   has_many :service_offering_node_service_credentials, :through => :service_offering_nodes
   has_many :service_offering_tags, :through => :service_offerings
-  has_many :service_instances
+  has_many :service_instances, :dependent => :destroy
   has_many :service_instance_service_credentials, :through => :service_instances
   has_many :service_instance_node_service_credentials, :through => :service_instance_nodes
-  has_many :service_instance_nodes
+  has_many :service_instance_nodes, :dependent => :destroy
   has_many :service_instance_node_service_credentials, :through => :service_instance_nodes
-  has_many :service_inventories
+  has_many :service_inventories, :dependent => :destroy
   has_many :service_inventory_tags, :through => :service_inventories
-  has_many :service_plans
+  has_many :service_plans, :dependent => :destroy
 
   # Tasks
   has_many :tasks, :dependent => :destroy
